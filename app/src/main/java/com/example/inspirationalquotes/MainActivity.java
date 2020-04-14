86ed40b20019c5d259b96ba2ee620e617630ca8d
Brian <blabbit321@gmail.com>
2020-04-14 11:02:31 +1000

    second commit

--- app/src/main/java/com/example/inspirationalquotes/MainActivity.java
+++ app/src/main/java/com/example/inspirationalquotes/MainActivity.java
@@ -23,29 +23,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Links XML elements to class variables
         mButton = (Button) findViewById(R.id.button);
         mQuote = (TextView) findViewById(R.id.quote);
+        // Generates a quote on start up
         generateQuote();
 
-
+        // Sets an OnClickListener that will help change the quote
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
+                // Quote is changed every time button is click
                 generateQuote();
 
 
             }
         });
     }
+    // method for code re-usability so that the quote generates when the app is started and when changed
     public void generateQuote() {
+        //Create Retrofit instance and parse the retrieved Json using Gson deserializer
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.chucknorris.io/").addConverterFactory(GsonConverterFactory.create()).build();
+        //Get service and call object for the request
         JokesService service = retrofit.create(JokesService.class);
         Call<Jokes> jokesCall = service.getJoke();
         jokesCall.enqueue(new Callback<Jokes>() {
             @Override
             public void onResponse(Call<Jokes> call, Response<Jokes> response) {
                 String jokes =response.body().getValue();
+                // Changes the quote encapsulated in quotation marks
                 mQuote.setText("\""+jokes+"\"");
             }
 
